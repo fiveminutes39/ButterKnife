416539fe2f3e9aed8096c56b12c93ebfa30083fa
fiveminute39 <five.minute39@gmail.com>
2020-08-14 13:05:55 +0200

    Initial commit

---
+++ src/main/java/com/example/butterknife/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.butterknife;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editText)
+    EditText editText;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
+        String text = editText.getText().toString();
+        textView.setText(text);
+    }
+}
